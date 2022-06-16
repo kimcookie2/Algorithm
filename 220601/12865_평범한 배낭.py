c58a2ca0312989e8de91687b3f26f73270d944a4
김준석 <jor9511@naver.com>
2022-06-16 20:47:19 +0900

    220616

--- 220601/12865_평범한 배낭.py
+++ 220601/12865_평범한 배낭.py
@@ -1,3 +1,30 @@
 import sys
 sys.stdin = open('input1.txt', 'r')
 
+# 물품의 수 N, 버틸 수 있는 무게 K
+N, K = map(int, input().split())
+objects = [0] * N
+
+# 계산 결과를 저장할 list
+d = [[0] * (K + 1) for _ in range(N)]
+
+for i in range(N):
+    objects[i] = list(map(int, input().split()))
+
+for i in range(N):
+    for j in range(K + 1):
+        weight = objects[i][0]
+        value = objects[i][1]
+
+        # 현재 배낭에 들어갈 수 있는 무게보다 현재 넣을 물건의 무게가 크다면 넣지 않는다.
+        if j < weight:
+            d[i][j] = d[i - 1][j]
+
+        # 그렇지 않다면 다음 중 더 높은 가치를 선택한다.
+        # 1. 현재 물건을 넣지않고 이전 배낭을 그대로 가지고 간다.
+        # 2. 현재 넣을 물건의 무게만큼 배낭에서 뺀다. 그리고 현재 물건을 넣는다.
+        else:
+            d[i][j] = max(d[i - 1][j], d[i - 1][j - weight] + value)
+
+print(d[N - 1][K])
+print(d)
--- 220613/1655_가운데를 말해요.py
+++ 220613/1655_가운데를 말해요.py
@@ -1,3 +1,24 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
+N = int(sys.stdin.readline())
+nums = []
+
+for i in range(N):
+    tmp = int(sys.stdin.readline())
+
+    if i == 0:
+        nums.append(tmp)
+        print(nums[0])
+        continue
+
+    for j in range(i):
+        if j == i - 1 or nums[j] >= tmp:
+            nums.insert(j, tmp)
+            break
+
+    center = int(i / 2)
+    if (i + 1) % 2 == 1:
+        print(nums[center])
+    else:
+        print(min(nums[center], nums[center + 1]))
